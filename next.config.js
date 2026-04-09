5b8670d04667dbeb9f358671b6cf9b13295f3a9b
Dispatch <dispatch@cartercodes.dev>
2026-04-08 20:59:30 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-edward-p-jessup-pa-lumberton-zzyh2v9hv',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
